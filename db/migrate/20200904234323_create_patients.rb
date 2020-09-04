745066abb64644bf3474556e5784249fd9e26b9b
albion9007 <68537855+albion9007@users.noreply.github.com>
2020-09-05 08:56:33 +0900

    patientsテーブルの作成

---
+++ db/migrate/20200904234323_create_patients.rb
@@ -0,0 +1,22 @@
+class CreatePatients < ActiveRecord::Migration[6.0]
+  def change
+    create_table :patients do |t|
+      t.integer :p_num,      null: false
+      t.string  :name,      null: false
+      t.text    :birth_date
+      t.string     :tel,   default: ""
+      t.string     :post_num,   default: ""
+      t.integer    :prefecture_id,                null: false
+      t.string     :city,        default: ""
+      t.string     :house_num,   default: ""
+      t.string     :apart_name,  default: ""
+      t.string    :key_person,   default: ""
+      t.string    :kp_tel,   default: ""
+      t.string    :main_disease,   null: false
+      t.string    :sub_disease,   default: ""
+      t.string    :main_doctor,   null: false
+      t.references :user, foreign_key: true
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_04_152123) do
+ActiveRecord::Schema.define(version: 2020_09_04_234323) do
+
+  create_table "patients", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "p_num", null: false
+    t.string "name", null: false
+    t.text "birth_date"
+    t.string "tel", default: ""
+    t.string "post_num", default: ""
+    t.integer "prefecture_id", null: false
+    t.string "city", default: ""
+    t.string "house_num", default: ""
+    t.string "apart_name", default: ""
+    t.string "key_person", default: ""
+    t.string "kp_tel", default: ""
+    t.string "main_disease", null: false
+    t.string "sub_disease", default: ""
+    t.string "main_doctor", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_patients_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -25,4 +46,5 @@ ActiveRecord::Schema.define(version: 2020_09_04_152123) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "patients", "users"
 end
