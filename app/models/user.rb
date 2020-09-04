edcbb3a7b7218e33de845168b3c054c180a8862d
albion9007 <68537855+albion9007@users.noreply.github.com>
2020-09-05 01:05:14 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :patient_users
+  has_many :patients, through: :patient_users
+
+  with_options presence: true do
+    validates :name, uniqueness: { case_sensitive: true }, length: { maximum: 15 }
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX, message: ''
+  end
 end
