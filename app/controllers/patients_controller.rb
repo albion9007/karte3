5683fbad9c5f8a237ba8ec5d8a44422f5315d709
albion9007 <68537855+albion9007@users.noreply.github.com>
2020-09-06 18:30:14 +0900

    病室内患者一覧表示（たたき台）

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -3,8 +3,10 @@ class PatientsController < ApplicationController
     @patient = Patient.new
   end
 
-  # def index
-  # end
+  def index
+    @patients = Patient.all.order("created_at DESC")
+  end
+
   def create
     @patient = Patient.new(patient_params)
     if @patient.save
@@ -22,6 +24,35 @@ class PatientsController < ApplicationController
   end
 end
 
-# def index
-#   @kartes = Karte.includes(:user).order("created_at DESC")
-# end
+
+
+
+
+# <% @items.each do |item|%>
+#   <li class='list'>
+#     <%= link_to item_path(item.id) do %>
+#     <div class='item-img-content'>
+#          <%= image_tag item.image, class:"item-img" %>
+#       <%# 商品が売れていればsold outの表示 %>
+#       <% if item.buy.present? %>
+#       <div class='sold-out'>
+#         <span>Sold Out!!</span>
+#       </div>
+#       <% end %>
+#       <%# //商品が売れていればsold outの表示 %>
+#     </div>
+#     <div class='item-info'>
+#       <h3 class='item-name'>
+#         <%= item.item_name %>
+#       </h3>
+#       <div class='item-price'>
+#         <span><%= item.price %>円<br>(税込み)</span>
+#         <div class='star-btn'>
+#           <%= image_tag "star.png", class:"star-icon" %>
+#           <span class='star-count'>0</span>
+#         </div>
+#       </div>
+#     </div>
+#     <% end %>
+#   </li>
+#   <% end %>
