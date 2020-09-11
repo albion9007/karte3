4c02e22695a9bf73101a7d2c4dbcb0f9eac06189
albion9007 <68537855+albion9007@users.noreply.github.com>
2020-09-11 13:21:07 +0900

    rubocop

--- app/controllers/observations_controller.rb
+++ app/controllers/observations_controller.rb
@@ -1,7 +1,7 @@
 class ObservationsController < ApplicationController
   before_action :set_patient, only: [:new, :create, :edit, :update]
   before_action :set_observation, only: [:edit, :update]
-  
+
   def new
     @observation = Observation.new
   end
@@ -17,33 +17,22 @@ class ObservationsController < ApplicationController
 
   def show
     observations = Observation.where(patient_id: params[:patient_id]).to_a
-    @observations = observations.sort_by{|o| o.time.delete(":").to_i}
-    @data = @observations.map  do |o|
-      if o.temperature.present?
-        [o.time, o.temperature]
-      end
+    @observations = observations.sort_by { |o| o.time.delete(':').to_i }
+    @data = @observations.map do |o|
+      [o.time, o.temperature] if o.temperature.present?
     end
     @data2 = @observations.map do |o|
-      if o.pulse.present?
-        [o.time, o.pulse]
-      end
+      [o.time, o.pulse] if o.pulse.present?
     end
     @data3 = @observations.map do |o|
-      if o.respiration.present?
-        [o.time, o.respiration]
-      end
+      [o.time, o.respiration] if o.respiration.present?
     end
     @data4 = @observations.map do |o|
-      if o.high_blood_pressure.present?
-        [o.time, o.high_blood_pressure]
-      end
+      [o.time, o.high_blood_pressure] if o.high_blood_pressure.present?
     end
     @data5 = @observations.map do |o|
-      if o.low_blood_pressure.present?
-        [o.time, o.low_blood_pressure]
-      end
+      [o.time, o.low_blood_pressure] if o.low_blood_pressure.present?
     end
-
   end
 
   # def edit
@@ -54,7 +43,7 @@ class ObservationsController < ApplicationController
     if @observation.update(observation_params) # バリデーションをクリアした時
       redirect_to patient_observation_path(@observation)
     else
-      render :edit  # バリデーションに弾かれた時
+      render :edit # バリデーションに弾かれた時
     end
   end
 
@@ -62,7 +51,7 @@ class ObservationsController < ApplicationController
 
   def observation_params
     params.require(:observation).permit(:temperature, :pulse, :respiration, :high_blood_pressure, :low_blood_pressure, :spo2, :food_intake,
-    :water_intake, :excresion, :ex_amount, :atten_sound, :atten_part, :sputum, :cough, :sleep, :time, :hainyou).merge(user_id: current_user.id, patient_id: params[:patient_id], user_name: current_user.name)
+                                        :water_intake, :excresion, :ex_amount, :atten_sound, :atten_part, :sputum, :cough, :sleep, :time, :hainyou).merge(user_id: current_user.id, patient_id: params[:patient_id], user_name: current_user.name)
   end
 
   def set_patient
